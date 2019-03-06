a71598ea8026d57e3ef805db6a0bf731b5c7b4cc
Tommy Tran <tranttommy95@gmail.com>
2019-03-06 15:25:24 -0800

    converDate function passed

--- test/make-template.test.js
+++ test/make-template.test.js
@@ -2,11 +2,11 @@ const test = QUnit.test;
 
 QUnit.module('make template literal');
 
-function makeTemplate() {
+function makeTemplate(quote) {
     const html = /*html*/`
     <li>
-        <p>An 'extremely credible source' has called my office and told me that Barack Obama's birth certificate is a fraud.</p>
-        <h2>August 6, 2012</h2>
+        <p>${quote.value}</p>
+        <h2>${quote.appeared_at}</h2>
     </li>
     `;
     const template = document.createElement('template');
@@ -22,10 +22,61 @@ test('create html template', assert => {
         <h2>August 6, 2012</h2>
     </li>
     `;
-
+    const quote = {
+        "appeared_at": "2012-08-06T00:00:00",
+        "created_at": "2016-11-14T02:07:54.408994",
+        "quote_id": "5i2eEJzdS9a0daHLzQheqw",
+        "tags": [
+            "Barack Obama"
+        ],
+        "updated_at": "2016-11-14T02:07:54.408994",
+        "value": "An 'extremely credible source' has called my office and told me that Barack Obama's birth certificate is a fraud.",
+        "_links": {
+            "self": {
+                "href": "/quote/5i2eEJzdS9a0daHLzQheqw"
+            }
+        },
+        "_embedded": {
+            "author": [{
+                "created_at": "2016-11-14T01:14:02.096776",
+                "bio": null,
+                "author_id": "wVE8Y7BoRKCBkxs1JkqAvw",
+                "name": "Donald Trump",
+                "slug": "donald-trump",
+                "updated_at": "2016-11-14T01:14:02.096776"
+            }],
+            "source": [{
+                "created_at": "2016-11-14T02:07:54.331382",
+                "filename": null,
+                "quote_source_id": "DusNnKwsQWu-osffgzgO_A",
+                "remarks": null,
+                "updated_at": "2016-11-14T02:07:54.331382",
+                "url": "https://twitter.com/realdonaldtrump/status/232572505238433794"
+            }]
+        }
+    };
     // Act
-    const result = makeTemplate();
+    const result = makeTemplate(quote);
 
     // Assert
     assert.htmlEqual(result, expected);
+});
+
+function convertDate(utcCode){
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const date = new Date(utcCode);
+    const niceDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    return niceDate;
+}
+
+
+
+test('convert date to nicer form', assert => {
+    // Arrange
+    const expected = 'August 6, 2012';
+    // Act
+    const result = convertDate("2012-08-06T00:00:00");
+    // Assert
+    assert.equal(result, expected);
 });
