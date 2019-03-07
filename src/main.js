b203e8509962263e530ca62b65be590cba395154
Payton Lower <paytown@me.com>
2019-03-07 10:45:46 -0800

    function written to have url control what is displayed

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { loadCharacters } from './character-component.js';
 import { characters } from './data.js';
 import './search-component.js';
 import { readFromQuery } from './hash-query.js';
+import { updateSearchTerm } from './search-component.js';
 
 loadCharacters(characters);
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
     const queryOptions = readFromQuery(query);
-    console.log(queryOptions);
+    updateSearchTerm(queryOptions.searchTerm);
 });
