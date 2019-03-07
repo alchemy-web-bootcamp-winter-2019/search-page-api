83294e06022cdb929c3d559302d620588dfffc50
Laura Badgley <l.badgleyy@gmail.com>
2019-03-07 15:12:06 -0800

    hashchange eventListener

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import characterData from '../data/data.js';
 import loadCharacterContainer from './make-character-template.js';
-// import { readFromQuery } from './hash-query.js';
+import { readFromQuery } from './hash-query.js';
 import './search-component.js';
+import './paging-component.js';
 
 loadCharacterContainer(characterData);
 
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(existingQuery);
+    console.log(queryOptions);
+});
