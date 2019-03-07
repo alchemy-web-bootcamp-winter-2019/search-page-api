308dd7a85d35e6dafedbbe4b8ae06b1e0f447d50
Tommy Tran <tranttommy95@gmail.com>
2019-03-06 17:07:27 -0800

    added hashchange event listener, captured query object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import quoteList from '../src/data.js';
 import loadQuotes from './make-template.js';
+import { readFromQuery } from '../src/hash-query.js';
 import './search-component.js';
 
 loadQuotes(quoteList);
+
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+
+    const queryOptions = readFromQuery(existingQuery);
+    console.log(queryOptions);
+});
