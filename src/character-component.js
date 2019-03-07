c0e426681df3328857f322825d760c369dbe62ec
Payton Lower <paytown@me.com>
2019-03-07 09:21:36 -0800

    wrote fn to loadCharacters in dom

--- src/character-component.js
+++ src/character-component.js
@@ -1,12 +1,21 @@
 export default function makeHtmlTemplate(characterInformation) {
     const template = document.createElement('template');
     const dom = `
-    <div class="character-div">
-        <h3>${characterInformation.name}</h3>
-    <img src="https://rickandmortyapi.com/api/character/avatar/${characterInformation.id}.jpeg" alt="${characterInformation.name}" class="character-thumbnail">
-        <p>Species: ${characterInformation.species}</p>
-    </div>
+        <div class="character-div">
+            <h3>${characterInformation.name}</h3>
+        <img src="https://rickandmortyapi.com/api/character/avatar/${characterInformation.id}.jpeg" alt="${characterInformation.name}" class="character-thumbnail">
+            <p>Species: ${characterInformation.species}</p>
+        </div>
     `;
     template.innerHTML = dom;
     return template.content;
+}
+
+const characterList = document.getElementById('character-list');
+
+export function loadCharacters(characters) {
+    characters.forEach(characterInformation => {
+        const dom = makeHtmlTemplate(characterInformation);
+        characterList.appendChild(dom);
+    });
 }
