da9ddb6dd3acecb3ab8490a95c9c715a83b2a461
Anna Ridenour <annamridenour@gmail.com>
2019-03-07 12:02:24 -0800

    created updatePageQuery through TDD

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -18,4 +18,35 @@ test('adding search to empty hash', assert => {
     const results = writeSearchToQuery(existingQuery, searchTerm);
     //assert
     assert.equal(results, 'searchTerm=human&page=1');
-})
+});
+
+test('updating search with new term', assert => {
+    //arrange
+    const existingQuery = 'searchTerm=human&page=1';
+    const searchTerm = 'alien';
+    
+    //act
+    const results = writeSearchToQuery(existingQuery, searchTerm);
+    //assert
+    assert.equal(results, 'searchTerm=alien&page=1');
+});
+    
+
+function updatePageQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+    return searchParams.toString();
+}
+
+test('changing page number without changing search term', assert => {
+    //arrange
+    const page = 3; 
+    const existingQuery = 'searchTerm=human&page=1';
+
+    //act
+    const results = updatePageQuery(existingQuery, page);
+    //assert
+    assert.equal(results, 'searchTerm=human&page=3');
+});
+
+
