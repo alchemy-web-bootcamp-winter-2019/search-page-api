7d6963ae43edd05a6136fec74a5c218c184f1b1d
Parker Helland <hellandp@gmail.com>
2019-03-07 09:58:06 -0800

    deleted junk function from limit-cats.js

--- src/limit-cats.js
+++ src/limit-cats.js
@@ -2,18 +2,11 @@ import { writeLimitToQuery } from './querys-components.js';
 
 const limitNode = document.getElementById('text-input');
 const generateNode = document.getElementById('generate-button');
-// const formNode = document.getElementById('input-form');
 
 generateNode.addEventListener('click', event => {
     event.preventDefault();
     const currentQuery = window.location.hash.slice(1);
     const catLimit = limitNode.value;
     const newQuery = writeLimitToQuery(currentQuery, catLimit);
-    window.location.has = newQuery;
-});
-
-window.addEventListener('hashchange', () => {
-    const currentQuery = window.location.hash;
-    writeLimitToQuery(currentQuery);
-    // console.log('current', );
+    window.location.hash = newQuery;
 });
