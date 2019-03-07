220785d070d0295fea7ad73b339ca9f70f64d253
Laura Badgley <l.badgleyy@gmail.com>
2019-03-07 15:25:21 -0800

    url updating to match searchBar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import loadCharacterContainer from './make-character-template.js';
 import { readFromQuery } from './hash-query.js';
 import './search-component.js';
 import './paging-component.js';
-
+import { updateSpeciesDropDown } from './search-component.js';
 loadCharacterContainer(characterData);
 
 window.addEventListener('hashchange', () => {
     const existingQuery = window.location.hash.slice(1);
     const queryOptions = readFromQuery(existingQuery);
     console.log(queryOptions);
+    updateSpeciesDropDown(queryOptions.species);
 });
--- src/search-component.js
+++ src/search-component.js
@@ -1,13 +1,16 @@
 import { writeSearchToQuery } from './hash-query.js';
 const searchForm = document.getElementById('form');
-const speciesDropdown = document.getElementById('species-dropdown');
+const speciesDropDown = document.getElementById('species-dropdown');
 
 searchForm.addEventListener('change', event => {
     event.preventDefault();
 
     const existingQuery = window.location.hash.slice(1);
-    const speciesChoice = speciesDropdown.value;
+    const speciesChoice = speciesDropDown.value;
     const newQuery = writeSearchToQuery(existingQuery, speciesChoice);
     window.location.hash = newQuery;
 });
 
+export function updateSpeciesDropDown(species) {
+    speciesDropDown.value = species;
+}
