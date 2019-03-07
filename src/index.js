f2649ef4d91e667a38e2f5eaf9a2bf84b0e9add9
Parker Helland <hellandp@gmail.com>
2019-03-07 10:23:14 -0800

    added event listeners to next and previous buttons to write to the query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import loadCats from './create-cat-component.js';
 import './limit-cats.js';
-
-
+import { writePageToQuery } from './querys-components.js';
 
 // const url = 'https://api.thecatapi.com/v1/images/search?limit=15&page=0&order=Desc';
+const nextButton = document.getElementById('next-button');
+const previousButton = document.getElementById('previous-button');
+
+let currentPageNumber = 1;
+
+nextButton.addEventListener('click', () => {
+    const existingQuery = window.location.hash.slice(1);
+    event.preventDefault();
+    currentPageNumber++;
+    const newQuery = writePageToQuery(existingQuery, currentPageNumber);
+    window.location.hash = newQuery;
+});
+
+previousButton.addEventListener('click', () => {
+    event.preventDefault();
+    const existingQuery = window.location.hash.slice(1);
+    currentPageNumber--;
+    const newQuery = writePageToQuery(existingQuery, currentPageNumber);
+    window.location.hash = newQuery;
+});
 
 fetch(url)
     .then(response => response.json())
