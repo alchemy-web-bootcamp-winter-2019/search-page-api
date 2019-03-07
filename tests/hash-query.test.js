5d774fbacbbb8c47915f959c6a048bf6f3306ea4
mcnadams <mcneilbo@oregonstate.edu>
2019-03-06 16:09:31 -0800

    TDD add search to existing query

--- tests/hash-query.test.js
+++ tests/hash-query.test.js
@@ -14,3 +14,14 @@ test('add search params to empty query', assert => {
 
     assert.equal(result, expected);
 });
+
+test('add search params to existing query', assert => {
+    const existingQuery = 'name=morty&page=1';
+    const searchTerm = 'rick';
+
+    const expected = 'name=rick&page=1';
+
+    const result = writeSearchToQuery(existingQuery, searchTerm);
+
+    assert.equal(result, expected);
+});
