aa51256c4732bd3c28214869f28b84d4e8798701
Alexandre Muniz <aom@puzzlezapper.com>
2019-03-06 16:28:21 -0800

    Added fetch from API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import loadCards from './list-component.js';
 import testArray from '../data/test-array.js';
 import './search-component.js';
+import { readFromQuery } from './hash-query.js';
+import makeSearchNameURL from './make-search-name-url.js';
 
-loadCards(testArray);
+// loadCards(testArray);
+
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+    const searchOptions = readFromQuery(existingQuery);
+    const url = makeSearchNameURL(searchOptions);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(body => loadCards(body.cards))
+        .catch(err => {
+            console.log(err);
+        });
+});
