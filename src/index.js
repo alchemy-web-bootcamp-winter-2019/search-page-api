5f936850ca587544aee9282e7e2f1ddcc8790271
emily baier <emilyrbaier@gmail.com>
2019-03-06 18:00:25 -0800

    on the way to having our patterns populate... just got the authentication for api to work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import data from './sample-object.js';
 import loadPatterns from './load-patterns.js';
 
+const query = encodeURIComponent('ding dong');
+//const page = 1;
+//const URL = 'https://api.ravelry.com/patterns/search.json?query=\'ding+dong\'&page=1';
 
+const username = 'read-4c64aafe518a851fe9f72f0fc442337b';
+const password = 'l13ub39WAF6WkObCfRJ4k9D450cdrAcMWlRWfjir';
+const url = 'https://cors-anywhere.herokuapp.com/https://api.ravelry.com/patterns/search.json?query=ding+dong';
+
+const auth = window.btoa(username + ':' + password);
+fetch(url, {
+    headers: {
+        Authorization: `Basic ${auth}`,
+        Origin: null
+    }
+})
+    .then(res => res.json())
+    .then(results => console.log(results));
 
 
 loadPatterns(data);
--- src/load-patterns.js
+++ src/load-patterns.js
@@ -2,6 +2,9 @@ import makePatternTemplate from './make-pattern-template.js';
 const patternDisplay = document.getElementById('pattern-display');
 
 export default function loadPatterns(data) {
+    // while(patternDisplay.children.length > 0) {
+    //     patternDisplay.lastElementChild.remove();
+    // }
     data.patterns.forEach(pattern => {
         const dom = makePatternTemplate(pattern);
         patternDisplay.appendChild(dom);
