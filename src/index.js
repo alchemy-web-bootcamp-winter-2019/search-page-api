eb9da5f7c14ca3b64668cef7040c5018a927af48
Parker Helland <hellandp@gmail.com>
2019-03-06 21:53:36 -0800

    travis passing?

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './limit-cats.js';
 
 
 
-// const url = 'https://api.thecatapi.com/v1/images/search?limit=15&page=0&order=Desc';
+const url = 'https://api.thecatapi.com/v1/images/search?limit=15&page=0&order=Desc';
 
 fetch(url)
     .then(response => response.json())
     .then(body => {
-        console.log('fetch body', body);
+        // console.log('fetch body', body);
         loadCats(body);
     });
