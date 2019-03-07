e6e4716d82d22147fbc68716da09ffe5ab9705cd
mcnadams <mcneilbo@oregonstate.edu>
2019-03-06 16:12:21 -0800

    TDD writePageToQuery

--- tests/hash-query.test.js
+++ tests/hash-query.test.js
@@ -25,3 +25,23 @@ test('add search params to existing query', assert => {
 
     assert.equal(result, expected);
 });
+
+QUnit.module('change page');
+
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+
+    return searchParams.toString();
+}
+
+test('write page to existing query', assert => {
+    const existingQuery = 'name=morty&page=1';
+    const page = 2;
+
+    const expected = 'name=morty&page=2';
+
+    const result = writePageToQuery(existingQuery, page);
+
+    assert.equal(result, expected);
+});
