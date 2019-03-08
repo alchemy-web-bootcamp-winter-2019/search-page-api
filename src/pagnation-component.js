0e7615632f0f31472280a9ccc3e9cd156e93296a
Parker Helland <hellandp@gmail.com>
2019-03-07 17:56:04 -0800

    fixed travis

--- src/pagnation-component.js
+++ src/pagnation-component.js
@@ -2,12 +2,12 @@ import { writePageToQuery } from './querys-components.js';
 
 const nextButton = document.getElementById('next-button');
 const previousButton = document.getElementById('previous-button');
-const button = document.getElementById('generate-button');
+// const button = document.getElementById('generate-button');
 
 let currentPageNumber = 1;
 previousButton.disabled = true;
 
-const textInputNode = document.getElementById('text-input');
+// const textInputNode = document.getElementById('text-input');
 
 // eslint-disable-next-line eqeqeq
 // if(textInputNode.value === false) {
